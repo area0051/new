0984e0c6bbf79e21a82558b4cb2ffadac2afccaf
area0051 <109328485+area0051@users.noreply.github.com>
2023-11-14 13:47:04 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,19 +8,21 @@ let sp = 1;
 let dead_flag = 0;
 let dead_t = 0;
 let score = 0;
-class Shadow{
-  constructor(){
+let rank = 'D'; // Initialize rank as 'D'
+
+class Shadow {
+  constructor() {
     this.x = 0;
     this.y = 0;
   }
 }
 
-class Effect{
-  constructor(){
+class Effect {
+  constructor() {
     this.x = 0;
     this.y = 0;
     this.t = 0;
-    this.message = "";
+    this.message = '';
   }
 }
 
@@ -32,46 +34,61 @@ function setup() {
   gameInit();
 }
 
+function updateRank() {
+  if (score >= 10 && score < 20) {
+    rank = 'C';
+  } else if (score >= 20 && score < 30) {
+    rank = 'B';
+  } else if (score >= 30 && score < 40) {
+    rank = 'A';
+  } else if (score >= 40) {
+    rank = 'S';
+  }
+}
+
 
 function draw() {
   background(255, 200, 0);
   t += sp;
-  
+
   textAlign(CENTER, CENTER);
   textSize(64);
   fill(100);
-  text(score, width/2, height/2 - 100);
+  text(score, width / 2, height / 2 - 100);
+
+  updateRank(); // Call the function to update the rank based on the score
+
   textSize(16);
   fill(100);
-  text("Rank D", width/2, height/2 - 60);
-  if(dead_flag == 0){
-    if(shot_flag == 1){
+  text('Rank ' + rank, width / 2, height / 2 - 60); // Display the rank
+
+  if (dead_flag == 0) {
+    if (shot_flag == 1) {
       x += (c_x - x) * 0.1;
       y += (c_y - y) * 0.1;
-      
-      if(dist(x, y, c_x, c_y) < 20){
-        
+
+      if (dist(x, y, c_x, c_y) < 20) {
         c_x = 600 - c_x;
         sp += 0.3;
         shot_flag = 0;
         score += 1;
-        
+
         addEffect(x, y, 1);
       }
     }
   }
-  
-  if(dead_flag == 0){
+
+  if (dead_flag == 0) {
     renderShadowList();
-    
+
     fill(0, 200, 200);
     ellipse(x, y, 20, 20);
-    
+
     addShadow(x, y);
-  }else{
+  } else {
     dead_t += 1;
-    if(dead_t < 60){
-      for(let i=0; i<10; i++){
+    if (dead_t < 60) {
+      for (let i = 0; i < 10; i++) {
         let angle = i * 36;
         let rad = radians(angle);
         let _x = cos(rad) * dead_t + x;
@@ -81,33 +98,33 @@ function draw() {
       }
     }
   }
- 
-  
+
   near_flag = 0;
-  for(let i=0; i<120; i++){
+  for (let i = 0; i < 120; i++) {
     let angle = i * 2.5 + t - 90;
     let rad = radians(angle);
     let _x = c_x + cos(rad) * 100;
     let _y = c_y + sin(rad) * 100;
-    
+
     let kyori = dist(_x, _y, x, y);
-    
-    if(kyori < 20){
+
+    if (kyori < 20) {
       dead_flag = 1;
     }
     fill(255);
     noStroke();
     ellipse(_x, _y, 20, 20);
   }
-  
-  if(dead_flag == 1){
+
+  if (dead_flag == 1) {
     textSize(32);
     fill(255);
-    text("- enter -", width/2, 350);
+    text('- enter -', width / 2, 350);
   }
-  
+
   renderEffectList();
 }
+
 
 function mousePressed(){
   if(dead_flag == 0){
